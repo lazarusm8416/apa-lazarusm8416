20e31bbf1be72394157672461bec1a32a986e8d5
lazarusm8416 <47994383+lazarusm8416@users.noreply.github.com>
2019-05-07 10:50:01 -0700

    Update Picture.java

--- pixlab/Picture.java
+++ pixlab/Picture.java
@@ -512,7 +512,36 @@ public void mirrorRectangle(int x1, int y1, int x2, int y2, boolean vertical){
       }
     }
   }
-  
+  public void edgeDetection2(int edgeDist){
+	  Pixel leftPixel = null;
+	  Pixel rightPixel = null;
+	  Pixel bottomPixel = null;
+	  Pixel[][] pixels = this.getPixels2D();
+	  Color rightColor = null;
+	  Color bottomColor = null;
+	    for (int row = 0; row < pixels.length; row++)
+	    {
+	      for (int col = 0; 
+	           col < pixels[0].length-1; col++)
+	      {
+	    	if(row != pixels.length-1) {
+	    		bottomPixel = pixels[row+1][col];
+	    	}
+	    	else {
+	    		bottomPixel = pixels[row][col];
+	    	}
+	        leftPixel = pixels[row][col];
+	        rightPixel = pixels[row][col+1];
+	        bottomColor = bottomPixel.getColor();
+	        rightColor = rightPixel.getColor();
+	        if (leftPixel.colorDistance(rightColor) > 
+	            edgeDist || (leftPixel.colorDistance(bottomColor)  > edgeDist))
+	          leftPixel.setColor(Color.BLACK);
+	        else
+	          leftPixel.setColor(Color.WHITE);
+	      }
+	    }
+  }
   
   /* Main method for testing - each class in Java can have a main 
    * method 
